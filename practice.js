edd60ee68a8a33cf22569d868aca0870f10f8f30
Ramchandra Goswami <162022360+Ramchandra456@users.noreply.github.com>
2024-08-12 16:56:18 +0000

    done

---
+++ practice.js
@@ -0,0 +1,17 @@
+// compare the price of pop corn 
+let size = "xl";
+if(size=="xl"){
+console.log("the price is 250");
+}
+else if(size=="l"){
+    console.log("the price is 200");
+    
+}
+else if(size=="m"){
+    console.log("the price is 150");
+    
+}
+else if(size=="s"){
+    console.log("the price is 100");
+    
+}
